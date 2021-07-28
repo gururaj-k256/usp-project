795c7f8b50e0a360f00fdb52d914994c5d0c6690
whorahulnayak <56409793+whorahulnayak@users.noreply.github.com>
2021-07-29 00:54:14 +0530

    Update user2.c

--- user2.c
+++ user2.c
@@ -1,10 +1,11 @@
-#include <sys/ipc.h>
-#include <sys/shm.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
-#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
 
 #define BUFFER_SIZE 200
 #define USER_READY 1
@@ -14,17 +15,17 @@ typedef struct buffer
 {
     char data[BUFFER_SIZE];
     int user_status;
-    int user1_pid;
-    int user2_pid;
+    int student_pid;
+    int teacher_pid;
 } Buffer;
 
 Buffer *shared_memory_ptr;
 
-void user2_sig_handler(int signal_id)
+void teacher_sig_handler(int signal_id)
 {
     if (signal_id == SIGUSR2)
     {
-        printf("User 1: ");
+        printf("Student: ");
         puts(shared_memory_ptr->data);
     }
 }
@@ -34,18 +35,18 @@ int main()
     key_t key = ftok("shared_mem_chat", 99);
     int shared_memory_id = shmget(key, sizeof(Buffer), IPC_CREAT | 0666);
     shared_memory_ptr = (Buffer *)shmat(shared_memory_id, NULL, 0);
-    shared_memory_ptr->user2_pid = getpid();
+    shared_memory_ptr->teacher_pid = getpid();
     shared_memory_ptr->user_status = USER_NOT_READY;
 
-    signal(SIGUSR2, user2_sig_handler);
+    signal(SIGUSR2, teacher_sig_handler);
     while (1)
     {
         sleep(1);
-        printf("User 2: ");
+        printf("Teacher: ");
         fgets(shared_memory_ptr->data, BUFFER_SIZE, stdin);
         shared_memory_ptr->user_status = USER_READY;
 
-        kill(shared_memory_ptr->user1_pid, SIGUSR1);
+        kill(shared_memory_ptr->student_pid, SIGUSR1);
 
         while (shared_memory_ptr->user_status == USER_READY)
             continue;
